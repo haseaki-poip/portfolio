4e5a58ed3cac9a1c7100475f04550ae02cc3c9b1
Hasegawa-Akito <h.akito1999@gmail.com>
2025-04-04 23:20:25 +0900

    Add analystic module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.scss";
 import "ress";
 
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={myfont.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
