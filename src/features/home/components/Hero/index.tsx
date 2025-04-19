06e971434c2fb6117445c570c01ad3de9084eaf0
Hasegawa-Akito <h.akito1999@gmail.com>
2025-04-20 00:14:45 +0900

    Change hero image from element to background

--- src/features/home/components/Hero/index.tsx
+++ src/features/home/components/Hero/index.tsx
@@ -1,27 +1,14 @@
-import Image from "next/image";
 import styles from "./style.module.scss";
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.hero__text_area}>
-        <div className={styles.hero__text_area_inner}>
+      <div className={styles.hero__contents}>
+        <div className={styles.hero__contents_inner}>
           <h1 className={styles.hero__title}>My Portfolio</h1>
           <p className={styles.hero__description}>
             created by <span>Akito Hasegawa</span>
           </p>
-        </div>
-      </div>
-      <div className={styles.hero__image_area}>
-        <div className={styles.hero__image_wrapper}>
-          <Image
-            src="/images/hero.png"
-            alt="hero image"
-            fill
-            className={styles.hero__image}
-            loading="eager"
-            priority
-          />
         </div>
       </div>
     </section>
